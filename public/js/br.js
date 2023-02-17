2141f36da5e331967af5ff7d377421af8c26c86a
Tina Huynh <tinamchuynh@gmail.com>
2023-02-17 12:37:17 -0800

    update translations

--- public/js/br.js
+++ public/js/br.js
@@ -1,9 +1,20 @@
 var search = document.querySelector(".btn-outline-success");
-var search_word = document.getElementsByClassName("search-word");
+var search_word = document.getElementById("search_word");
+var display_section = document.querySelector(".display-section");
 
-search.onclick = function() {
-    $.getJSON("https://api.funtranslations.com/translate/braille/unicode.json?text=" + search_word.innerHTML, function (data) {
-        console.log(data.contents.translated);
+
+search.onclick = function () {
+    console.log(search_word.value);
+
+    $.getJSON("https://api.funtranslations.com/translate/braille/unicode.json?text=" + search_word.value, function (data) {
+        console.log(data);
     });
 };
 
+function display_search_word(translated) {
+    for (var i = 0; i < translated.length; i++) {
+        var p = document.createElement("p");
+        p.innerHTML = translated[i].key()
+        display_section.appendChild(p);
+    }
+}
