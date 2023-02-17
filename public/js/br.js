021e950c2e8878f8730e61ef06b0fdcf60230a80
Tina Huynh <tinamchuynh@gmail.com>
2023-02-17 12:21:06 -0800

    translate words

--- public/js/br.js
+++ public/js/br.js
@@ -1,3 +1,9 @@
-$.getJSON("https://api.funtranslations.com/translate/braille/unicode.json?text=hello", function(data) {
-    console.log(data.contents.translated);
-});
+var search = document.querySelector(".btn-outline-success");
+var search_word = document.getElementsByClassName("search-word");
+
+search.onclick = function() {
+    $.getJSON("https://api.funtranslations.com/translate/braille/unicode.json?text=" + search_word.innerHTML, function (data) {
+        console.log(data.contents.translated);
+    });
+};
+
