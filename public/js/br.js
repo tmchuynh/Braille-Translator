00ae07dca790960d33bdef4e5b11cfad0df7294c
Tina Huynh <tinamchuynh@gmail.com>
2023-02-18 14:34:01 -0800

    takes special keys into consideration

--- public/js/br.js
+++ public/js/br.js
@@ -1,14 +1,19 @@
 var search = document.querySelector(".btn-outline-success");
 var search_word = document.getElementById("search_word");
 var display_section = document.querySelector(".display-section");
+let excludedKeys = [16, 17, 18, 225, 9, 27, 107, 109, 110, 37, 38, 39, 40, 20];
 
 search_word.onkeyup = function () {
     var input = search_word.value;
+    input = input.toLowerCase();
     console.log(input);
 
     /* Checking if the input is empty. If it is, it will clear the display section and return. */
     if (input === "") {
         display_section.innerHTML = "";
+        return;
+    }
+    else if (excludedKeys.includes(event.keyCode)) {
         return;
     }
     else if (event.keyCode == 8) {
