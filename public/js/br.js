6def6b3c21105216a70081ee0362f839c1325c29
Tina Huynh <tinamchuynh@gmail.com>
2023-02-18 14:08:32 -0800

    translate from images

--- public/js/br.js
+++ public/js/br.js
@@ -2,19 +2,22 @@ var search = document.querySelector(".btn-outline-success");
 var search_word = document.getElementById("search_word");
 var display_section = document.querySelector(".display-section");
 
-
 search.onclick = function () {
-    console.log(search_word.value);
+    var input = search_word.value;
+    console.log(input);
 
-    $.getJSON("https://api.funtranslations.com/translate/braille/unicode.json?text=" + search_word.value, function (data) {
-        console.log(data);
-    });
-};
-
-function display_search_word(translated) {
-    for (var i = 0; i < translated.length; i++) {
-        var p = document.createElement("p");
-        p.innerHTML = translated[i].key()
-        display_section.appendChild(p);
+    if (input === "") {
+        display_section.innerHTML = "";
+        return;
+    } else {
+        for (var i = 0; i< input.length; i++) {
+            var img = document.createElement("img");
+            if (input[i] === " ") { 
+                img.setAttribute("src", "public/imgs/space.png");
+            } else {
+                img.setAttribute("src", "public/imgs/" + input[i] + ".png");
+            }
+            display_section.appendChild(img);
+        }
     }
 }
